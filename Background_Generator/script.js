c59a5755d084fbf14dc7b476b6ad8a24eba0d350
nfluk <nickylukic@gmail.com>
2023-06-23 11:30:31 +0200

    step one in snippet done. not correct colors yet

--- Background_Generator/script.js
+++ Background_Generator/script.js
@@ -1,5 +1,23 @@
-let inputColor1 = document.querySelector('.color1');
+let btn = document.querySelector('.btn');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let h3 = document.querySelector('h3');
 
-inputColor1.addEventListener('click', () => {
-  console.log(inputColor1.value);
+btn.addEventListener('click', () => {});
+
+color1.addEventListener('click', () => {
+  addCodeSnippet();
 });
+
+color2.addEventListener('click', () => {
+  addCodeSnippet();
+});
+
+function addCodeSnippet() {
+  h3.innerText = '';
+  h3.appendChild(
+    document.createTextNode(
+      `background: linear-gradient(to right, ${color1.value}, ${color2.value});`
+    )
+  );
+}
