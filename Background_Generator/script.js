5bfc36704e4c92633ef037b2e7024141c9884ddc
nfluk <nickylukic@gmail.com>
2023-06-26 17:58:00 +0200

    cleaned out old comments

--- Background_Generator/script.js
+++ Background_Generator/script.js
@@ -17,7 +17,6 @@ btn.addEventListener('click', () => {
 });
 
 color1.addEventListener('input', () => {
-  // body.style.backgroundImage = 'linear-gradient(to right, #eaa4e6, #00bb00)';
   addCodeSnippet();
 });
 
@@ -31,38 +30,3 @@ function addCodeSnippet(direction = 'right') {
   h3.appendChild(document.createTextNode(`background: ${gradient});`));
   body.style.backgroundImage = gradient;
 }
-
-// WORKING:
-
-// let btn = document.querySelector('.btn');
-// let color1 = document.querySelector('.color1');
-// let color2 = document.querySelector('.color2');
-// let h3 = document.querySelector('h3');
-// let body = document.body;
-// let direction = 'right';
-
-// btn.addEventListener('click', () => {
-//   if (direction === 'right') {
-//     direction = 'left';
-//     return addCodeSnippet(direction);
-//   }
-//   if (direction !== 'right') {
-//     direction = 'right';
-//     addCodeSnippet(direction);
-//   }
-// });
-
-// color1.addEventListener('input', () => {
-//   body.style.backgroundImage = 'linear-gradient(to right, #eaa4e6, #00bb00)';
-//   addCodeSnippet();
-// });
-
-// color2.addEventListener('input', () => {
-//   addCodeSnippet();
-// });
-
-// function addCodeSnippet(direction = 'right') {
-//   let gradient = `linear-gradient(to ${direction}, ${color1.value}, ${color2.value}`;
-//   h3.innerText = '';
-//   h3.appendChild(document.createTextNode(`background: ${gradient});`));
-// }
